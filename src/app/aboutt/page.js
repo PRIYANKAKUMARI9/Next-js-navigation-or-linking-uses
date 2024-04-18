7102795655a81523f6ac1aaabed341a5624fd670
pk <pkji@.com>
2024-04-18 07:53:27 +0530

    first commit

---
+++ src/app/aboutt/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const aboutt=()=>{
+    return(
+        <div>
+            <h1>this is aboutt page</h1>
+            <Link href="/">go to home page</Link>
+        </div>
+    )
+}
+
+export default aboutt
---
+++ src/app/loginn/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const hello=()=>{
+    return(
+        <div>
+            <h1>this is loginn page.js</h1>
+            <Link href="/">go to Home page</Link>
+        </div>
+    )
+}
+export default hello
