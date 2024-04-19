b77b6a35c59ac73558ddd6e32ae1793a8384e96d
pk <pkji@.com>
2024-04-19 06:59:50 +0530

    first commit

--- src/app/aboutt/page.js
+++ src/app/aboutt/page.js
@@ -5,6 +5,13 @@ const aboutt=()=>{
         <div>
             <h1>this is aboutt page</h1>
             <Link href="/">go to home page</Link>
+
+            <br/>
+            <br/>
+
+            <Link href="/aboutt/aboutstudent">Go to aboutstudent</Link><br/><br/>
+            <Link href="/aboutt/aboutCollege">go to aboutCollege</Link>
+
         </div>
     )
 }
